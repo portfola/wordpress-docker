262a58102ce8f84235dd14d267944f6c796e3104
Rindy Portfolio <rindy@tadpole.cc>
2025-05-01 15:41:13 -0400

    Create local wp-content as mount of container wp-content

--- create-wp-site.sh
+++ create-wp-site.sh
@@ -9,12 +9,18 @@ mkdir -p "$INSTANCE_NAME"
 cd "$INSTANCE_NAME"
 
 # Copy Docker files
-cp ./dockerfile Dockerfile
-cp ./docker-entrypoint-custom.sh docker-entrypoint-custom.sh
-cp ./docker-compose.yml docker-compose.yml
+cp ../dockerfile Dockerfile
+cp ../docker-entrypoint-custom.sh docker-entrypoint-custom.sh
+cp ../docker-compose.yml docker-compose.yml
+
+# Remove any existing wp-content directory if it exists
+rm -rf wp-content
 
 # Start containers
 docker-compose up -d
 
-echo "WordPress site starting at http://localhost"
+echo "WordPress site starting at http://localhost:8080"
 echo "Admin login: jerry / garcia"
+echo ""
+echo "Container is initializing. WordPress and plugins are being installed..."
+echo "This may take a minute. The wp-content directory will be populated from the container."
